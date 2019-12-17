609ab0c5f0dcc5f5129ba9742abdb70b293f1327
Mohit Shridhar <mohit.appi@gmail.com>
2019-12-17 19:52:23 +0800

    download link fix

--- data/download_data.sh
+++ data/download_data.sh
@@ -10,7 +10,7 @@ then
 elif [ "$1" = "json_feat" ]
 then
     echo "Downloading JSONs and Resnet18 feats ..."
-    wget --load-cookies /tmp/cookies.txt "https://drive.google.com/a/cs.washington.edu/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://drive.google.com/a/cs.washington.edu/uc?export=download&id=1PBEF5qW_TKLCFYd2Dt7_SQkkgNC9jPFd' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1PBEF5qW_TKLCFYd2Dt7_SQkkgNC9jPFd" -O $FILENAME && rm -rf /tmp/cookies.txt
+    wget --load-cookies /tmp/cookies.txt "https://drive.google.com/a/cs.washington.edu/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://drive.google.com/a/cs.washington.edu/uc?export=download&id=1kY2jAKuAL6J56QPSKnuu4BbRnLg0WX37' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1kY2jAKuAL6J56QPSKnuu4BbRnLg0WX37" -O $FILENAME && rm -rf /tmp/cookies.txt
 elif [ "$1" = "json_feat_imgs" ]
 then
     echo "Downloading JSONs, Resnet18 feats, and Images ..."
@@ -19,14 +19,6 @@ then
 else
     echo "Failed: Usage download_data.sh json | json_feat | json_feat_imgs"
     exit 1
-fi
-
-# remove json folder from repo
-REPO_JSON_FOLDER='./json_2.1.0'
-if [ -d "$REPO_JSON_FOLDER" ]; then
-  # Take action if $DIR exists. #
-  echo "Removing json_2.1.0 from repo"
-  rm -r $REPO_JSON_FOLDER
 fi
 
 # unzip and delete zip
