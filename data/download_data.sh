da7ce90c164bd1f17bb6808e48e2a2f2ab24ba88
Mohit Shridhar <mohit.appi@gmail.com>
2019-12-18 08:22:54 +0800

    download link fix

--- data/download_data.sh
+++ data/download_data.sh
@@ -15,7 +15,7 @@ elif [ "$1" = "json_feat_imgs" ]
 then
     echo "Downloading JSONs, Resnet18 feats, and Images ..."
     echo "TODO"
-    wget --load-cookies /tmp/cookies.txt "https://drive.google.com/a/cs.washington.edu/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://drive.google.com/a/cs.washington.edu/uc?export=download&id=TODO' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=TODO" -O $FILENAME && rm -rf /tmp/cookies.txt
+    wget --load-cookies /tmp/cookies.txt "https://drive.google.com/a/cs.washington.edu/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://drive.google.com/a/cs.washington.edu/uc?export=download&id=1xe_asKo6Kb7nQ3c-agPqnhGT7SDJdn84' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1xe_asKo6Kb7nQ3c-agPqnhGT7SDJdn84" -O $FILENAME && rm -rf /tmp/cookies.txt
 else
     echo "Failed: Usage download_data.sh json | json_feat | json_feat_imgs"
     exit 1
