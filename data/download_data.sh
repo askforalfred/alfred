5e697aaf0a8325e4146d30c5572fe2ba4ae8e7b8
Mohit Shridhar <mohit.appi@gmail.com>
2019-12-18 12:34:17 +0800

    download link fix

--- data/download_data.sh
+++ data/download_data.sh
@@ -1,24 +1,30 @@
 #!/bin/bash
 
-# filename
-FILENAME=alfred_data.zip
 
 if [ "$1" = "json" ]
 then
+
     echo "Downloading JSONs ..."
     wget https://storage.googleapis.com/alfred_dataset/data/json_2.1.0.zip
+    unzip json_2.1.0.zip && rm json_2.1.0.zip
+    echo "data folder: json_2.1.0"
+
 elif [ "$1" = "json_feat" ]
 then
+
     echo "Downloading JSONs and Resnet18 feats ..."
     wget https://storage.googleapis.com/alfred_dataset/data/json_feat_2.1.0.zip
+    unzip json_feat_2.1.0.zip && rm json_feat_2.1.0.zip
+    echo "data folder: json_feat_2.1.0"
+
 elif [ "$1" = "full" ]
 then
+
     echo "Downloading JSONs, Resnet18 feats, Images, Videos, and PDDL states ..."
-    wget --load-cookies /tmp/cookies.txt "https://drive.google.com/a/cs.washington.edu/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://drive.google.com/a/cs.washington.edu/uc?export=download&id=1xe_asKo6Kb7nQ3c-agPqnhGT7SDJdn84' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1xe_asKo6Kb7nQ3c-agPqnhGT7SDJdn84" -O $FILENAME && rm -rf /tmp/cookies.txt
+    echo "TODO"
+    echo "data folder: full_2.1.0"
+
 else
     echo "Failed: Usage download_data.sh json | json_feat | full"
     exit 1
 fi
-
-# unzip and delete zip
-unzip $FILENAME && rm $FILENAME
