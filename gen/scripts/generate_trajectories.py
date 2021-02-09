c283dd1ee7d74b611fea546d99b8f443b97c6a92
Jiasen Lu <echosenm@gmail.com>
2021-02-09 01:51:24 -0800

    update

--- gen/scripts/generate_trajectories.py
+++ gen/scripts/generate_trajectories.py
@@ -1,7 +1,7 @@
 import os
 import sys
-sys.path.append(os.path.join('/mnt/raid00/jiasen/alfred'))
-sys.path.append(os.path.join('/mnt/raid00/jiasen/alfred', 'gen'))
+sys.path.append(os.path.join('/home/jiasenl/code/alfred'))
+sys.path.append(os.path.join('/home/jiasenl/code/alfred', 'gen'))
 
 import time
 import multiprocessing as mp
@@ -434,7 +434,7 @@ def main(args, thread_num=0):
     print("Loaded %d known failed tuples" % len(fail_traj))
 
     # create env and agent
-    env = ThorEnv(x_display='0.%d' %(thread_num % 8))
+    env = ThorEnv(x_display='0.%d' %(thread_num % 2))
 
     game_state = TaskGameStateFullKnowledge(env)
     agent = DeterministicPlannerAgent(thread_id=0, game_state=game_state)
@@ -458,18 +458,16 @@ def main(args, thread_num=0):
 
     n_until_load_successes = args.async_load_every_n_samples
     print_successes(succ_traj)
-    # task_sampler = sample_task_params(succ_traj, full_traj, fail_traj,
-    #                                   goal_candidates, pickup_candidates, movable_candidates,
-    #                                   receptacle_candidates, scene_candidates)
+    task_sampler = sample_task_params(succ_traj, full_traj, fail_traj,
+                                      goal_candidates, pickup_candidates, movable_candidates,
+                                      receptacle_candidates, scene_candidates)
     
     # main generation loop
     # keeps trying out new task tuples as trajectories either fail or suceed
     while True:
-        # if True:
-        #     if True:
     # for _ in range(20):
         for ii, json_path in enumerate(glob.iglob(os.path.join(alfred_dataset_path, "**", "traj_data.json"), recursive=True)):
-            if ii % args.num_threads == thread_num:
+            # if ii % args.num_threads == thread_num:
             # if ii == 5:
                 sampled_task = json_path.split('/')[-3].split('-')
                 # sampled_task = next(task_sampler)
